5ee917a18df82aad2652e78fcaa0b1aadcd407f3
20120095 <hungngojan25@gmail.com>
2023-05-17 23:00:42 +0700

    connect mongodb

--- index.js
+++ index.js
@@ -2,8 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const route = require('./routes');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
+//connect to mongodb
+
+const uri = process.env.mongoUri;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 const app = express();
 const port = process.env.PORT;
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const { MongoClient } = require('mongodb');
 
 router.get('/', (req, res) => {
-  res.status(200).send('hello');
+  // Replace the uri string with your MongoDB deployment's connection string.
+  const uri = process.env.mongoUri;
+
+  const client = new MongoClient(uri);
+
+  async function run() {
+    try {
+      const database = client.db('BflightsDB');
+      const movies = database.collection('collection1');
+      const findResult = await movies.find({}).toArray();
+      res.status(200).json(findResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  run();
 });
 
 module.exports = router;

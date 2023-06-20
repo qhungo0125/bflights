7df3a60ba4c5e8470968998e796ced31c21de2b7
20120095 <hungngojan25@gmail.com>
2023-06-21 00:21:15 +0700

    split page in ticket classes

--- controllers/ticketClassController.js
+++ controllers/ticketClassController.js
@@ -1,26 +1,34 @@
-const { ticketClassMethod } = require("../models/ticketClass")
+const { ticketClassMethod } = require('../models/ticketClass');
 
 class TicketClassController {
     checkExistedId = async (id) => {
         try {
-            const res = await ticketClassMethod.getById(id)
-            if (res)
-                return true
-            else
-                return false
+            const res = await ticketClassMethod.getById(id);
+            if (res) return true;
+            else return false;
         } catch (error) {
-            return false
+            return false;
         }
-
-    }
+    };
     all = async (req, res) => {
+        const { page = 1, perPage = 5 } = req.query;
         try {
-            const ticketClasses = await ticketClassMethod.getAll()
-            res.status(200).json(ticketClasses)
+            const ticketClasses = await ticketClassMethod.getAll();
+
+            const startIdx = (page - 1) * perPage;
+            const endIdx = page * perPage;
+
+            const tcData = ticketClasses.slice(startIdx, endIdx);
+
+            if (tcData.length == 0) {
+                return res.status(200).json([]);
+            }
+
+            return res.status(200).json(tcData);
         } catch (error) {
-            res.status(500).json({ error: error.message })
+            res.status(500).json({ error: error.message });
         }
-    }
+    };
 }
 
-module.exports = new TicketClassController
+module.exports = new TicketClassController();
